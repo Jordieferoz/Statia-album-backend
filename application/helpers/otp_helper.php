1b940f44b8925bcf1d9ce54cb6cc19394ebc2cf2
Rohit Sahu <rohitsahu728@gmail.com>
2023-05-12 22:02:46 +0530

    mail removed

--- application/helpers/otp_helper.php
+++ application/helpers/otp_helper.php
@@ -1,29 +1,29 @@
 <?php
 
 function sendsms($email, $vars) {
-    $from = 'statiagallery@gmail.com';
-    $ci = &get_instance();
-    $config = array(
-        'protocol' => 'smtp',
-        'smtp_host' => 'ssl://smtp.googlemail.com',
-        'smtp_port' => 465,
-        'smtp_user' => $from,
-        'smtp_pass' => 'Stati@dmin',
-        'mailtype' => 'html',
-        'charset' => 'iso-8859-1',
-        'wordwrap' => TRUE
-    );
+    // $from = 'statiagallery@gmail.com';
+    // $ci = &get_instance();
+    // $config = array(
+    //     'protocol' => 'smtp',
+    //     'smtp_host' => 'ssl://smtp.googlemail.com',
+    //     'smtp_port' => 465,
+    //     'smtp_user' => $from,
+    //     'smtp_pass' => 'Stati@dmin',
+    //     'mailtype' => 'html',
+    //     'charset' => 'iso-8859-1',
+    //     'wordwrap' => TRUE
+    // );
 
-    $message = 'Hello,\nPlease use this OTP for your account. \n\n OTP: ' . $vars['var1'];
-    $ci->load->library('email', $config);
-    $ci->email->set_newline("\r\n");
-    $ci->email->from($from);
-    $ci->email->to($email);
-    $ci->email->subject('Mail from Statia Pictures');
-    $ci->email->message($message);
-    if ($ci->email->send()) {
-        echo 'Email sent.';
-    } else {
-        show_error($ci->email->print_debugger());
-    }
+    // $message = 'Hello,\nPlease use this OTP for your account. \n\n OTP: ' . $vars['var1'];
+    // $ci->load->library('email', $config);
+    // $ci->email->set_newline("\r\n");
+    // $ci->email->from($from);
+    // $ci->email->to($email);
+    // $ci->email->subject('Mail from Statia Pictures');
+    // $ci->email->message($message);
+    // if ($ci->email->send()) {
+    //     echo 'Email sent.';
+    // } else {
+    //     show_error($ci->email->print_debugger());
+    // }
 }
